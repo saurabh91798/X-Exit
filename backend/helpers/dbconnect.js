47fd997bca876c6f905e7a0d00bd35f64b29b8bf
K.Saurabh Rao <saurabh91798@gmail.com>
2024-11-22 21:56:41 +0530

    added backend and frontend

---
+++ backend/helpers/dbconnect.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGO_URI
+export default mongoose
+  .connect(uri)
+  .then(() => console.log("DB connection established"));
---
+++ frontend/src/helpers/isAlreadyAuthenticated.js
@@ -0,0 +1,21 @@
+import { authenticatedInstance } from "./axiosInstances.js";
+
+export default async () => {
+  try {
+    Object.assign(authenticatedInstance.defaults, { headers: { Authorization: localStorage.getItem('token') } });
+    // verify token
+    let {
+      data: { roles },
+    } = await authenticatedInstance.get("/api/auth");
+    localStorage.setItem("roles", JSON.stringify(roles));
+    return {
+      roles,
+      isAuthenticated: true,
+    };
+  } catch (e) {
+    console.log(">", e);
+    return {
+      isAuthenticated: false,
+    };
+  }
+};
